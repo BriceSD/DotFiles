0ed1fc66d5b3e59710e8e752c6e66b36103ad33e
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-04-08 11:03:08 +0200

    feat: added git Save/WIP/Undo/Wipe aliases

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local Brice_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local Brice_Fugitive = vim.api.nvim_create_augroup("Brice_Fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
@@ -12,14 +12,14 @@ autocmd("BufWinEnter", {
         end
 
         local bufnr = vim.api.nvim_get_current_buf()
-        local opts = {buffer = bufnr, remap = false}
+        local opts = { buffer = bufnr, remap = false }
         vim.keymap.set("n", "<leader>p", function()
             vim.cmd.Git('push')
         end, opts)
 
         -- rebase always
         vim.keymap.set("n", "<leader>P", function()
-            vim.cmd.Git({'rebase'})
+            vim.cmd.Git({ 'rebase' })
         end, opts)
 
         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
@@ -29,5 +29,32 @@ autocmd("BufWinEnter", {
 
         vim.keymap.set("n", "<leader>gr", "<cmd>diffget //2<CR>");
         vim.keymap.set("n", "<leader>ga", "<cmd>diffget //3<CR>");
+
+        -- Manage changing branch with WIP/Undo commit
+        -- See https://nicolaiarocci.com/git-worktree-vs-git-savepoints/
+        -- Commit all files (even untracked ones) with SAVEPOINT message
+        vim.keymap.set("n", "<leader>sa", function()
+            vim.cmd.Git('add -A')
+            vim.cmd.Git('commit -m "SAVEPOINT"')
+        end, opts)
+
+        -- Commit only tracked files with WIP message
+        vim.keymap.set("n", "<leader>st", function()
+            vim.cmd.Git('add -u')
+            vim.cmd.Git('commit -m "WIP"')
+        end, opts)
+
+        -- Undo last commit
+        vim.keymap.set("n", "<leader>su", function()
+            vim.cmd.Git('reset HEAD^ --mixed')
+        end, opts)
+
+        -- adds changes in the working tree to a WIPE SAVEPOINT commit, then it wipes the commit
+        -- still accessible through "reflog"
+        vim.keymap.set("n", "<leader>sx", function()
+            vim.cmd.Git('add -A')
+            vim.cmd.Git('commit -qm "WIPE SAVEPOINT"')
+            vim.cmd.Git('reset HEAD~1 --hard')
+        end, opts)
     end,
 })
