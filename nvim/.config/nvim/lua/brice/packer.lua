4aacafdbb9bfcc61c2fc1180e9bb02bd4d7a286f
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-04-11 11:27:20 +0200

    feat: Added neotree

--- nvim/.config/nvim/lua/brice/packer.lua
+++ nvim/.config/nvim/lua/brice/packer.lua
@@ -59,6 +59,16 @@ return require('packer').startup(function(use)
     use('mbbill/undotree')
 
 
+    -- Browse filesystem in a tree
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
+    }
 
     -- Git
     use('tpope/vim-fugitive')             -- git integration/wrapper
