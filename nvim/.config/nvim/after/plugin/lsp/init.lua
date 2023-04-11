75b306b2f2fb55c48502cb2f4643f654fe2090e3
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-04-11 12:36:15 +0200

    refactor: moved/merged lsp files in its own folder

--- nvim/.config/nvim/after/plugin/lsp/init.lua
+++ nvim/.config/nvim/after/plugin/lsp/init.lua
@@ -2,51 +2,114 @@
 --TrySource 'lsp.typescript'
 --TrySource 'lsp.viml'
 
-vim.diagnostic.config {
-    virtual_text = true,
-}
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+    'tsserver',
+    'rust_analyzer',
+    'eslint',
+    'sumneko_lua',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Enter>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
 
 local function buf_set_option(...)
     vim.api.nvim_buf_set_option(0, ...)
 end
 
 buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
--- stylua: ignore start
-vim.keymap.set("n", 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>') -- Goto Declaration
---vim.keymap.set("n", 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')  -- Goto Definition
 
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---vim.keymap.set("N", 'K', '<cmd>lua vim.lsp.buf.hover()<CR>') -- Well, K is K
---TrySource 'lsp.rust' -- Have this after K mapping so it gets remapped
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
 
---vim.keymap.set("n", 'gr', 'lsp_references')                                         -- Goto References
-vim.keymap.set("n", 'gI', 'lsp_implementations')                                    -- Goto Implementations
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts) -- Goto Declaration
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    --vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    --    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    --    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    --vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    --vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
 
---vim.keymap.set("n", '<Leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>') -- Code Rename
-vim.keymap.set({ 'v', 'n' }, '<Leader>ca', vim.lsp.buf.code_action)
 
-vim.keymap.set("i", '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>') -- Show Signature
+    --vim.keymap.set("n", 'gr', 'lsp_references')                                         -- Goto References
+    --vim.keymap.set("n", 'gI', '<cmd> lua vim.lsp.implementations') -- Goto Implementations
+    --vim.keymap.set("n", '<Leader>cr', function() vim.lsp.buf.rename() end, opts) -- Code Rename
+    vim.keymap.set({ 'v', 'n' }, '<Leader>ca', vim.lsp.buf.code_action)
 
-vim.keymap.set("n", '<Leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
-vim.keymap.set("n", '<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
-vim.keymap.set("n", '<Leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+    --vim.keymap.set("i", '<C-s>', function() vim.lsp.buf.signature_help() end, opts) -- Show Signature
 
--- Diagnostics:
+    vim.keymap.set("n", '<Leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+    vim.keymap.set("n", '<Leader>wa', function() vim.lsp.buf.add_workspace_folder() end, opts)
+    vim.keymap.set("n", '<Leader>wr', function() vim.lsp.buf.remove_workspace_folder() end, opts)
 
-vim.keymap.set("n", '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>') -- Diagnostics
-vim.keymap.set("n", 'dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')         -- Diagnostics Previous
-vim.keymap.set("n", 'dn', '<cmd>lua vim.diagnostic.goto_next()<CR>')         -- Diagnostics Next
--- vim.keymap.set("n", '<Leader>dl', 'diagnostics')
--- vim.keymap.set("N", '<Leader>ld', '<cmd>Telescope diagnostics<CR>') -- List Diagnotics
-vim.keymap.set("n", '<Leader>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>') -- List Location List
-vim.keymap.set("n", '<Leader>lf', '<cmd>lua vim.diagnostic.setqflist()<CR>')  -- List quickFix
+    -- Diagnostics:
+    --vim.keymap.set("n", '<Leader>dl', function() vim.diagnostic.open_float() end, opts) -- Diagnostics
+    vim.keymap.set("n", 'dp', function() vim.diagnostic.goto_prev() end, opts)          -- Diagnostics Previous
+    vim.keymap.set("n", 'dn', function() vim.diagnostic.goto_next() end, opts)          -- Diagnostics Next
+    --vim.keymap.set("n", '<Leader>dl', 'diagnostics')
+    --vim.keymap.set("n", '<Leader>ld', '<cmd>Telescope diagnostics<CR>')                 -- List Diagnotics
+    vim.keymap.set("n", '<Leader>ll', function() vim.diagnostic.setloclist() end, opts) -- List Location List
+    vim.keymap.set("n", '<Leader>lf', function() vim.diagnostic.setqflist() end, opts)  -- List quickFix
+end)
+
+
+vim.diagnostic.config({
+    virtual_text = true
+})
+
 
 local whichkey = require 'which-key'
 whichkey.register {
     g = {
         name = 'LSP', -- optional group name
         D = { '[LSP] Goto Declaration' },
-        I = { '[LSP] Show Implementations' },
+        --I = { '[LSP] Show Implementations' },
         K = { '[LSP] Display Hover Info' },
         d = { '[LSP] Goto definition' },
     },
@@ -69,9 +132,9 @@ whichkey.register {
         d = { '[Diagnostics] Display Line Diagnostics' },
         l = {
             name = 'Diagnostics',
-            d = { '[Diagnostics] List Diagnostics' },
+            --d = { '[Diagnostics] List Diagnostics' },
             f = { '[Diagnostics] Quickfix List' },
-            l = { '[Diagnostics] Location List' },
+            --l = { '[Diagnostics] Location List' },
         },
     },
     d = {
@@ -82,8 +145,10 @@ whichkey.register {
     v = {
         name = 'View',
         r = {
-            r = { 'References'},
+            r = { 'References' },
         }
     }
 }
+
+lsp.setup()
 -- stylua: ignore end
