95661be2b673ae13a9534bc6c2fbaa0bc6fb11da
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-04-17 12:24:07 +0200

    feat(nvim): WIP, improved leap plugin settings

--- nvim/.config/nvim/after/plugin/leap.lua
+++ nvim/.config/nvim/after/plugin/leap.lua
@@ -1,2 +1,25 @@
 require('leap').add_default_mappings()
-
+-- require('leap').opts = {
+--     max_phase_one_targets = nil,
+--     highlight_unlabeled_phase_one_targets = false,
+--     max_highlighted_traversal_targets = 10,
+--     case_sensitive = false,
+--     equivalence_classes = { ' \t\r\n', },
+--     substitute_chars = {},
+--     labels = { 's', 't', 'r', 'd', 'g', 'h', 'n', 'a', 'e', 'i', 'l', 'm', 'o', 'u', '/', 'y' },
+--     special_keys = {
+--         repeat_search = '<enter>',
+--         next_phase_one_target = '<enter>',
+--         next_target = { '<enter>', ';' },
+--         prev_target = { '<tab>', ',' },
+--         next_group = '<space>',
+--         prev_group = '<tab>',
+--         multi_accept = '<enter>',
+--         multi_revert = '<backspace>',
+--     }
+-- }
+require('leap').opts.safe_labels = { 'n', 'h', 'g', 'r', 'e', 't', 's', 'm', 'u', 'l', '/', 'y', '(', ',', 'f', '[', ']' }
+require('leap').opts.case_sensitive = false
+require('leap').opts.special_keys.multi_accept = { '<enter>' }
+require('leap').opts.special_keys.next_target = { ';' }
+require('leap').opts.special_keys.prev_target = { ',' }
