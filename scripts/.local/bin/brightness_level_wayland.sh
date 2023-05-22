6c3bd3808c5b14a64e0cf10591595a6150fe2ff9
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-05-22 11:45:25 +0200

    feat(wayland): migrating from X11/i3 to wayland/sway

---
+++ scripts/.local/bin/brightness_level_wayland.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# https://aur.archlinux.org/packages/ddcci-driver-linux-dkms#comment-908688
+# https://bbs.archlinux.org/viewtopic.php?id=255080
+
+devvideos=$(brightnessctl -l | grep "class 'backlight'" | cut -f '2' -d ' ' | sed "s/'//g")
+
+if [[ $(brightnessctl -d ddcci8) == "Device 'ddcci8' not found" ]]; then
+    sudo modprobe ddcci
+    sudo modprobe dcci_backlight
+    echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/i2c-8/new_device
+elif [ ! -d "/dev/bus/ddcci/8" ]; then
+    sudo modprobe ddcci
+    sudo modprobe dcci_backlight
+    echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/i2c-8/new_device
+else
+    echo "Monitor brightness is already loaded."
+    sudo dmesg | rg ddcci
+fi
+
+case $1 in
+    -) val="U"  ;;
+    +) val="A"  ;;
+esac
+# `exec brillo -el -$val 3`
+for devvideo in $devvideos
+do
+    echo $devvideo
+   `exec brightnessctl -d $devvideo s 3%$1 &`
+done
