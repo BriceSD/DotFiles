51c098a6046c460a8eb3f898cb52fa33759d7c06
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-08-02 14:01:09 +0200

    feat(i3): brightness ddcci update

--- scripts/.local/bin/brightness_level_wayland.sh
+++ scripts/.local/bin/brightness_level_wayland.sh
@@ -2,17 +2,18 @@
 
 # https://aur.archlinux.org/packages/ddcci-driver-linux-dkms#comment-908688
 # https://bbs.archlinux.org/viewtopic.php?id=255080
+# `ddcutil detect` t know which i2c number
 
 devvideos=$(brightnessctl -l | grep "class 'backlight'" | cut -f '2' -d ' ' | sed "s/'//g")
 
-if [[ $(brightnessctl -d ddcci8) == "Device 'ddcci8' not found" ]]; then
+if [[ $(brightnessctl -d ddcci5) == "Device 'ddcci5' not found" ]]; then
     sudo modprobe ddcci
     sudo modprobe dcci_backlight
-    echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/i2c-8/new_device
-elif [ ! -d "/dev/bus/ddcci/8" ]; then
+    echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/i2c-5/new_device
+elif [ ! -d "/dev/bus/ddcci/5" ]; then
     sudo modprobe ddcci
     sudo modprobe dcci_backlight
-    echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/i2c-8/new_device
+    echo 'ddcci 0x37' | sudo tee /sys/bus/i2c/devices/i2c-5/new_device
 else
     echo "Monitor brightness is already loaded."
     sudo dmesg | rg ddcci
@@ -22,9 +23,9 @@ case $1 in
     -) val="U"  ;;
     +) val="A"  ;;
 esac
-# `exec brillo -el -$val 3`
-for devvideo in $devvideos
-do
-    echo $devvideo
-   `exec brightnessctl -d $devvideo s 3%$1 &`
-done
+`exec brillo -el -$val 3`
+# for devvideo in $devvideos
+# do
+#     echo $devvideo
+#    `exec brightnessctl -d $devvideo s 3%$1 &`
+#done
