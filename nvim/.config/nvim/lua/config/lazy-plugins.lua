0de06affc8fd945532c4975fcd072be7a03e3c21
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-10-08 12:55:13 +0200

    feat(nvim): go lsp config

--- nvim/.config/nvim/lua/config/lazy-plugins.lua
+++ nvim/.config/nvim/lua/config/lazy-plugins.lua
@@ -213,6 +213,23 @@ local plugins = {
     -- Java related plugins
     'mfussenegger/nvim-jdtls',
 
+    -- Go related plugins
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("go").setup()
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", 'gomod' },
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
+
+
     -- Debugging
     {
         'mfussenegger/nvim-dap',
