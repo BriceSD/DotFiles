cb899f3e265ff755935fb5a51612374f80636fd5
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-11-01 20:46:12 +0100

    feat(nvim): using k instead of K to hover informations

--- nvim/.config/nvim/after/plugin/lsp/init.lua
+++ nvim/.config/nvim/after/plugin/lsp/init.lua
@@ -105,7 +105,7 @@ lsp_zero.on_attach(function(client, bufnr)
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts) -- Goto Declaration
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "k", function() vim.lsp.buf.hover() end, opts)
     --vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     --vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
